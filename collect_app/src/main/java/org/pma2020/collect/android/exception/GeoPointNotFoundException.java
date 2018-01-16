d0e6ab8d19fcb81d841230bc0c63eb1e71445aa7
James <jameskpringle@gmail.com>
2018-01-16 11:49:52 -0500

    Change package path to org.pma2020 instead of org.odk

---
+++ collect_app/src/main/java/org/pma2020/collect/android/exception/GeoPointNotFoundException.java
@@ -0,0 +1,14 @@
+package org.pma2020.collect.android.exception;
+
+public class GeoPointNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public GeoPointNotFoundException (String msg) {
+		super(msg);
+	}
+
+}
